3ea008a9df8cd080e799155371151c8cef39c9d7
sala 305 <sala305@poliacademico.edu.loc>
2021-12-01 07:54:14 -0500

    clase del 1 de diciembre

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const chalk = require("chalk");
-const express = require("express");
+const chalk = require('chalk')
+const express = require('express')
 
-const app = express();
+const app = express()
 
-app.listen(3000, ()=>{
-    console.log("servidor corriendo")
+app.get('/home',(req,res)=>{
+    res.send('<h1>Hola mundo</h1>')
+})
+
+app.get('/page',(req,res)=>{
+    res.send({ message:"hola mundo"})
+})
+
+app.listen(3000, () => {
+  console.log(chalk.redBright('servidor corriendo'))
 })
