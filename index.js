0cc7304cc8d6c590275bcdb844d9c4eff68fe58c
sala 305 <sala305@poliacademico.edu.loc>
2021-12-06 07:51:20 -0500

    clase 6 diciembre

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const chalk = require('chalk')
 const express = require('express')
 
+const usuarios = [];
+
 const app = express()
 
-app.get('/home',(req,res)=>{
+app.post('/user',express.json(),(req,res)=>{
+  usuarios.push(req.body); 
+  console.log(req.body);
+  res.send({ message:"este es el post"})  
+})
+
+app.get('/home',(req,res)=>{ 
     res.send('<h1>Hola mundo</h1>')
+})
+
+app.get('/user',(req,res)=>{ 
+  res.send(usuarios);
 })
 
 app.get('/page',(req,res)=>{
